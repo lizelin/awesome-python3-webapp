99536a0da6ae0c132f1d8629fa7cf3547d27b63c
lizelin <lizelin@hotmail.com>
2020-05-08 23:53:12 +0800

    Day-13

--- www/handlers.py
+++ www/handlers.py
@@ -16,6 +16,7 @@ from apis import APIError, APIResourceNotFoundError, APIValueError
 
 from models import User, Comment, Blog, next_id
 from config import configs
+from apis import APIPermissionError, Page
 
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs['session']['secret']
@@ -181,4 +182,30 @@ def manage_create_blog():
         'id': '',
         'action': '/api/blogs'
     }
-    
+
+@get('/api/blogs')
+async def api_blogs(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, blogs={})
+    blogs = await Blog.findAll(orderBy = 'created_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, blogs=blogs)
+
+@get('/manage/blogs')
+def manage_blogs(*, page='1'):
+    return {
+        '__template__': 'manage_blogs.html',
+        'page_index': get_page_index(page)
+    }
+
+def get_page_index(page_str):
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError as e:
+        pass
+    if p < 1:
+        p = 1
+    return p
